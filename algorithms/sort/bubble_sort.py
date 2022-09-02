c197a5d9161baf293a65b2072efbe5fbc036d9af
johnlew01 <103500399+johnlew01@users.noreply.github.com>
2022-09-02 20:42:10 +0800

    Update bubble sort
    Time complexity  O(n^2)

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -1,35 +1,33 @@
-"""
-
-https://en.wikipedia.org/wiki/Bubble_sort
-
-Worst-case performance: O(N^2)
-
-If you call bubble_sort(arr,True), you can see the process of the sort
-Default is simulation = False
-
-"""
-
-
-def bubble_sort(arr, simulation=False):
-    def swap(i, j):
-        arr[i], arr[j] = arr[j], arr[i]
-
+def bubbleSort(arr):
     n = len(arr)
-    swapped = True
-    
-    iteration = 0
-    if simulation:
-        print("iteration",iteration,":",*arr)
-    x = -1
-    while swapped:
-        swapped = False
-        x = x + 1
-        for i in range(1, n-x):
-            if arr[i - 1] > arr[i]:
-                swap(i - 1, i)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
                 swapped = True
-                if simulation:
-                    iteration = iteration + 1
-                    print("iteration",iteration,":",*arr)
-                    
-    return arr
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+         
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
+ 
+ 
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+ 
+bubbleSort(arr)
+ 
+print("Sorted array is:")
+for i in range(len(arr)):
+    print("% d" % arr[i], end=" ")
